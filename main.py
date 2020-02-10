84df6d1399ea99d12976159f8b7642ebd1271894
icebreakeris <43800401+icebreakeris@users.noreply.github.com>
2020-02-10 11:04:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #pylint: disable=no-member
 
+import sys
 import numpy as np
 import cv2
 import imutils
@@ -33,8 +34,8 @@ class PlateScanner:
         #self.width = width
         #self.height = height
         self.show_steps = show_steps
-        self.morph_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(17,4))
-        self.plate_kernel = np.ones((5,5), np.uint8)
+        self.morph_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(20,4))
+        self.plate_kernel = np.ones((5,), np.uint8)
         self.contrast_kernel = np.array([[-1,0,1],[-2,0,2],[1,0,1]])
         self.plate_candidates = []
 
@@ -49,25 +50,35 @@ class PlateScanner:
             print("[-] File not found. Enter the correct url and try again.") 
             return
 
+        self.image = cv2.imread(self.url) 
+        self.image = imutils.resize(self.image, width=600)
+
         self.preprocess_image()
-        
+        #self.find_roi()
 
-    def find_roi(self):
+    def set_roi(self):
+        (y, x) = self.image.shape[:2]
 
-        pass
+        p1 = (int(x/4), int(2*y/4))
+        p2 = (int(x*4/5), int(2*y))
+
+        #cv2.rectangle(self.image, p1, p2, (255, 0, 0), 2)
+        roi = self.image[p1[1]:p2[1], p1[0]:p2[0], :]
+        return roi
+
     def enhance(self, image):
         return cv2.filter2D(image, -1, self.contrast_kernel)
 
     def preprocess_image(self):
-        self.image = cv2.imread(self.url)
-
-        self.image = imutils.resize(self.image, width = 600)
+        start = time.time()
         
+        if self.show_steps: cv2.imshow("Original image", self.image)
+
+        self.image = self.set_roi()
+        #self.image = imutils.resize(self.image, width=600)
         #r = cv2.selectROI(self.image) #for testing purposes (testing successful 20/23 plates found using roi)
         
-        #img_crop = self.image[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
-
-        if self.show_steps: cv2.imshow("Original image", self.image)
+        #self.image = self.image[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
 
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
@@ -111,11 +122,14 @@ class PlateScanner:
             candidate_num = candidate_num + 1 #debug
             candidate = cv2.minAreaRect(a)
 
+            #box = cv2.boxPoints(candidate)
+            #points = np.int0(box)
+            #cv2.drawContours(self.image, [points], 0, (255, 2, 10), 2)
+
             #(x,y), (width, height), angle = candidate
 
             #check whether the found rectangle meets the size requirements of a plate
             if self.check_size(candidate, candidate_num):
-                
                 box = cv2.boxPoints(candidate)
                 points = np.int0(box)
 
@@ -123,40 +137,50 @@ class PlateScanner:
     
                 if (x > 0 and y > 0 and w > h): #possibly put w > h into check_size()?
                     new_img = self.image[y:y + h, x:x + w]
-                    if self.check_edge_density(new_img) > 0.5:
+                    if self.check_edge_density(new_img) > 0.475:
                         cv2.imshow(f"cropped_plate{candidate_num}.png", new_img)
+
                         processed_plate = self.preprocess_plate(new_img)
-                        #d = pytesseract.image_to_data(processed_plate, output_type=Output.DICT)
+
+                        predicted_plate = self.get_plate_string(processed_plate)
+
+                        #print("Predicted plates: ")
+                        #for a in predicted_plate:
+                            #print(a)
 
                         cv2.drawContours(self.image, [points], 0, (255, 2, 10), 2)
-                        test = pytesseract.image_to_string(processed_plate, lang="eng", config=self.ocr_config)
-                        print(test)
-                        cv2.putText(self.image, f"{test}", (int(box[0][0]), int(box[0][1]) + 20), cv2.FONT_HERSHEY_DUPLEX, 0.9, (255, 255, 255), 2)
-                    #self.plate_candidates.append(candidate)
-
+                        cv2.putText(self.image, predicted_plate, (int(box[0][0]), int(box[0][1]) + 20), cv2.FONT_HERSHEY_DUPLEX, 0.9, (255, 255, 255), 2)
+                    
         if self.show_steps: cv2.imshow("last image", self.image)
 
+        end = time.time()
+        print(f"Time elapsed: {int((end-start)*1000)}ms")
         cv2.waitKey(0)
  
     #takes cut out plate img 
     def check_edge_density(self, plate_img):
+        #refactor
+
         gray = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
         _, plate_thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        white_pixels = 0
+        cv2.imshow("edge_den", plate_thresh)
 
+        white_count = 0
         for x in range(plate_thresh.shape[0]):
             for y in range(plate_thresh.shape[1]):
                 if plate_thresh[x][y] == 255:
-                    white_pixels += 1
+                    white_count += 1
 
-        edge_density = float(white_pixels)/(plate_thresh.shape[0]*plate_thresh.shape[1])
+        edge_density = float(white_count)/(plate_thresh.shape[0]*plate_thresh.shape[1])
         print(f"edge_density of candidate: {edge_density}")
+
         return edge_density
 
     def preprocess_plate(self, plate_img): 
         #preprocess plate image and get ready for ocr 
         plate_img = cv2.resize(plate_img, (500, 125))
+        #mask = np.zeros(plate_img.shape, dtype=np.uint8)
         gray = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
         
         #gray = cv2.bitwise_not(gray)
@@ -169,35 +193,49 @@ class PlateScanner:
         erosion = cv2.erode(opening, self.plate_kernel, iterations=1)
         dilated = cv2.dilate(erosion, self.plate_kernel, iterations=1)
 
+        contours, _ = cv2.findContours(plate_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
-        
-        #plate_thresh = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-
-        """
-        coords = np.column_stack(np.where(plate_thresh > 0))
-        angle = cv2.minAreaRect(coords)[-1]
-
-        if angle < -45:
-            angle = -(90 + angle)
-        else:
-            angle = -angle
-        
-        (h, w) = plate_thresh.shape[:2]
-        center = (w // 2, h//2)
-        m = cv2.getRotationMatrix2D(center, angle, 1.0)
-        rotated = cv2.warpAffine(plate_thresh, m, (w,h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-        """
-
-        cv2.imshow("preprocessed_plate", dilated)
-        
-        cv2.waitKey(0)
+        cv2.imshow("preprocessed_plate2", dilated)
 
         return dilated
 
+    def get_key_by_value(self, dictionary, value): 
+        for a in dictionary.items(): 
+            if a[1] == value:
+                return a[0]
+
+    def string_has_numbers(self, string): 
+        return any(i.isdigit() for i in string)
+
     def get_plate_string(self, img): 
+        """
+        confusing_chars = {'B' : '8', 'D': '0', 'E': '3', 'G' :'6', 'I' : '1', 'S' : '5', 'T' : '7'}
+        
+        candidate_plates = []
         #use ocr to get the string 
-        pass
+        #use uk plate rules to determine whether the plate is correct
+
+        #next two characters can only be numbers
+
+        plate_data = list(pytesseract.image_to_string(img, lang="eng", config=self.ocr_config))
+        original_plate = "".join(plate_data)
+
+        if self.string_has_numbers("".join(plate_data[:2])):
+            for i in plate_data[:2]:
+                if i in confusing_chars.keys() or i in confusing_chars.values():
+                    plate_data[plate_data.index(i)] = self.get_key_by_value(confusing_chars, i)
+            candidate_plates.append("".join(plate_data))
+
+
+        for i in plate_data[2:4:]:
+            if i in confusing_chars.keys(): 
+                plate_data[plate_data.index(i)] = confusing_chars.get(i)
+        candidate_plates.append("".join(plate_data))
+
+        candidate_plates.append(original_plate)
+        """
+
+        return pytesseract.image_to_string(img, config=self.ocr_config)
 
     def check_size(self, candidate, candidate_num):
         control_aspect = 4.68 # 520mm / 111mm (standard uk number plate size)
@@ -232,5 +270,6 @@ class PlateScanner:
 
 
 if __name__ == "__main__":
-    PlateScanner("plates/17.jpg", r"C:\Users\minda\AppData\Local\Tesseract-OCR\tesseract.exe", True).scan_plate()
+    if len(sys.argv) > 1:
+        PlateScanner(sys.argv[1], r"C:\Users\minda\AppData\Local\Tesseract-OCR\tesseract.exe", True).scan_plate()
 
